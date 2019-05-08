2033a19530d1ee7165a07b2afa89633c2b5dd747
yalimei <398408225@qq.com>
2019-05-08 15:59:45 +0800

    edit

--- weixin/server.php
+++ weixin/server.php
@@ -9,7 +9,7 @@ $options = include 'config.php';
 $app = new Application($options);
 $server = $app->server;
 $server->setMessageHandler(function ($message) {
-    return  "回复【$message】:亲，你来了，欢迎关注【易书在线】！";
+    return  "回复【{$message}】:亲，你来了，欢迎关注【易书在线】！";
 });
 
 $response = $server->serve();
