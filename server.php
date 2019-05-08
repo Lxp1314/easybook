00f31063f5ed3a3127c2a202ccfafae6e4f004a1
yalimei <398408225@qq.com>
2019-05-08 15:17:07 +0800

    edit

--- server.php
+++ server.php
@@ -6,14 +6,16 @@ use EasyWeChat\Foundation\Application;
 
 $options = [
     'debug'  => true,
-    //测试号
-//    'app_id' => 'wxa88af71505f895da',
-//    'secret' => 'cf05973f5465279b2249ed983ffe19cc',
-//    'token'  => 'mytest',
 
-    'app_id' => 'wx7c58cc4ee296d6d7',
-    'secret' => 'aa56d5f56c8cd378832cd5fbaec53b54',
-    'token'  => 'wuyun',
+    //测试号
+   'app_id' => 'wxa88af71505f895da',
+   'secret' => 'cf05973f5465279b2249ed983ffe19cc',
+   'token'  => 'mytest',
+
+    //易书在线
+    // 'app_id' => 'wx7c58cc4ee296d6d7',
+    // 'secret' => 'aa56d5f56c8cd378832cd5fbaec53b54',
+    // 'token'  => 'wuyun',
 
     // 'aes_key' => null, // 可选
 
