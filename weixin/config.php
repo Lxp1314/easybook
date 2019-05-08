7ace9c5a6ffabd8a1aa95d5a5c808f155f878b80
yalimei <398408225@qq.com>
2019-05-08 15:17:52 +0800

    edit

---
+++ weixin/config.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'debug'  => true,
+
+    //测试号
+    'app_id' => 'wxa88af71505f895da',
+    'secret' => 'cf05973f5465279b2249ed983ffe19cc',
+    'token'  => 'mytest',
+
+    //易书在线
+    // 'app_id' => 'wx7c58cc4ee296d6d7',
+    // 'secret' => 'aa56d5f56c8cd378832cd5fbaec53b54',
+    // 'token'  => 'wuyun',
+
+    // 'aes_key' => null, // 可选
+
+    'log' => [
+        'level' => 'debug',
+        'file'  => '/source/easybook/logs/easywechat.log', // XXX: 绝对路径！！！！
+    ],
+
+];
---
+++ weixin/server.php
@@ -0,0 +1,18 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use EasyWeChat\Foundation\Application;
+
+$options = include 'config.php';
+
+$app = new Application($options);
+$server = $app->server;
+$server->setMessageHandler(function ($message) {
+    return "亲，你来了，欢迎关注【易书在线】！";
+});
+
+$response = $server->serve();
+
+// 将响应输出
+$response->send(); 
