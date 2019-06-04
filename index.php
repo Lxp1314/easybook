7794040331e5fa548ba3048beed62f0079075a15
雅里梅 <398408225@qq.com>
2019-06-04 17:17:14 +0800

    edit

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ $jsApis = [
     
     <button onclick="chooseImage()">选择</button>
     <button onclick="previewImage()">预览</button>
-    <button onclick="uploadImage()">上传<button>
+    <button onclick="uploadImage(0)">上传<button>
     <div id="info"></div>
 </body>
 </html>
