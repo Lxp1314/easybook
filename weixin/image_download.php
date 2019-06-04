c84d90dbdac424bbf01e99270393e18eefce1e9d
雅里梅 <398408225@qq.com>
2019-06-04 19:08:52 +0800

    edit

--- weixin/image_download.php
+++ weixin/image_download.php
@@ -2,7 +2,7 @@
 
 class ImageDownload{
     public static function download($media_id){
-        $app = require './app.php';
+        $app = require __DIR__.'/./app.php';
         $content = $app->material_temporary->getStream($media_id);
         $path = '/storages/images/uploads/' . $media_id . '.jpg';
         file_put_contents('..' . $path, $contents);
