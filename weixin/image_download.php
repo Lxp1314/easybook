0b9202e11219ee41694872d9fa4bf4e6e025c85d
雅里梅 <398408225@qq.com>
2019-06-04 19:31:57 +0800

    edit

--- weixin/image_download.php
+++ weixin/image_download.php
@@ -4,10 +4,10 @@ class ImageDownload{
     public static function download($media_id){
         $app = require __DIR__.'/./app.php';
         $temporary = $app->material_temporary;
-        $temporary->download($media_id, "../storages/images/uploads/", "abc.jpg");
-        $content = $temporary->getStream($media_id);
-        $path = '/storages/images/uploads/' . $media_id . '.jpg';
-        file_put_contents('..' . $path, $contents);
-        return 'http://weixin.windmax.cn' . $path;
+        $temporary->download($media_id, "../storages/images/uploads/", $media_id.".jpg");
+        // $content = $temporary->getStream($media_id);
+        // $path = '/storages/images/uploads/' . $media_id . '.jpg';
+        // file_put_contents('..' . $path, $contents);
+        return 'http://weixin.windmax.cn/storages/images/uploads/' . $media_id . ".jpg";
     }
 }
