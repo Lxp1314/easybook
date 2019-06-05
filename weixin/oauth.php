0d6e064eb5afdd9c753bad92d0314ced9f5f421d
雅里梅 <398408225@qq.com>
2019-06-05 15:15:04 +0800

    edit

--- weixin/oauth.php
+++ weixin/oauth.php
@@ -5,9 +5,15 @@ class Oauth{
      * 返回微信授权用户信息
      */
     public static function login($call_back, $base = false){
-        $session_id = $base ? 'wechat_user_base' : 'wechat_user';
+        $session_id = 'wechat_user';
+        $scope = $base ? 'snsapi_base' : 'snsapi_userinfo';
+        $newOauth = false;//重新发起授权
+        if(empty($_SESSION['scope']) || $_SESSION['scope'] !== $scope){
+            $_SESSION['scope'] = $scope;
+            $newOauth = true;
+        }
         // 未登录
-        if (empty($_SESSION[$session_id])) {
+        if ($newOauth || empty($_SESSION[$session_id])) {
             // 设置回调地址
             $_SESSION['target_url'] = $call_back;
             $app = require __DIR__.'/./app.php';
