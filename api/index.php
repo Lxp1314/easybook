ae9c54960c271cd271368be0022f490ac8c4940c
雅里梅 <398408225@qq.com>
2019-06-05 18:32:21 +0800

    edit

--- api/index.php
+++ api/index.php
@@ -10,4 +10,18 @@ if($action === 'wx_image_upload'){
     require __DIR__.'/../weixin/image_download.php';
     $url = ImageDownload::download($media_id);
     responseJson(200, 'ok', $url);
+}elseif($action === 'base64_upload'){
+    $media_id = $_REQUEST['media_id'];
+    $image = $_POST['base64'];
+    if (strstr($image,",")){
+        // $image = explode(',',$base64);
+        $image = explode(',',$image)[1];
+    }
+    $savePath = __DIR__."/../storages/images/uploads/".time().'.png';
+    $r = file_put_contents($savePath, base64_decode($image));
+    if(!$r){
+        responseJson(400, 'error', '上传失败');
+    }else{
+        responseJson(200, 'ok', $savePath);
+    }
 }
