7f7907e71cb28a71f548a22c4dca910d91ef2494
雅里梅 <398408225@qq.com>
2019-06-05 14:42:44 +0800

    edit

--- userinfo.php
+++ userinfo.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/weixin/oauth.php';
 if(isWechat()){
     $base = !empty($_GET['base']);
     $user = Oauth::login('/userinfo.php', $base);
-    echo json_encode($user);
+    echo json_encode($user, JSON_UNESCAPED_UNICODE);
 }else{
     echo '非微信端';
 }
